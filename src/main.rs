68fba47c673c53da1ee963ed4b22b5446125a946
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2021-08-27 00:34:34 -0400

    remove input handling logic since windows_subsystem breaks

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,28 @@ use configparser::ini::Ini;
 use std::env;
 use std::io;
 
-fn update_profile(user: String, profile: String) {
+fn update_profile(user: &String, profile: &String) {
     let mut config = Ini::new_cs();
-    let profile_path = format!("C:/Games/f1-profiles/{}.ini", profile);
-    let config_path = format!("C:/Games/{}/User/Config/GCPadNew.ini", user);
+    let profile_path = &format!("C:/Games/f1-profiles/{}.ini", profile);
+    let config_path = &format!("C:/Games/{}/User/Config/GCPadNew.ini", user);
 
     let profile = config
-        .load(&profile_path)
-        .expect(format!("No config file found at path {}", &profile_path).as_str())
+        .load(profile_path)
+        .expect(format!("No config file found at path {}", profile_path).as_str())
         .get("Profile")
         .expect("The section 'Profile' does not exist in the config file. Exiting.")
         .clone();
 
     config
-        .load(&config_path)
-        .expect(format!("No config file found at path {}", &config_path).as_str());
+        .load(config_path)
+        .expect(format!("No config file found at path {}", config_path).as_str());
 
-    config.get_mut_map().insert("GCPad1".to_string(), profile);
+    config.get_mut_map().insert("GCPad1".to_owned(), profile);
 
-    config.write(&config_path).expect(
+    config.write(config_path).expect(
         format!(
             "Error while writing to configuration file with path {}",
-            &config_path
+            config_path
         )
         .as_str(),
     );
@@ -33,21 +33,9 @@ fn update_profile(user: String, profile: String) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (user, profile) = match &args[1..] {
-        [user, profile] => (user.to_owned(), profile.to_owned()),
-        _ => {
-            let input: Vec<String> = get_line("Load controller config. Format: ${user} ${profile}")
-                .split(" ")
-                .map(|s| String::from(s))
-                .collect();
-            match &input[..] {
-                [user, profile, ..] => Ok((user.to_owned(), profile.to_owned())),
-                _ => Err("Not enough parameters to load configuration"),
-            }
-            .unwrap()
-        }
-    };
-
+    let error_msg = "Not enough arguments provided. Exiting";
+    let user = args.get(1).expect(error_msg);
+    let profile = args.get(2).expect(error_msg);
     update_profile(user, profile);
 }
 
